bea4663ff18061e854eb7805c337914cea4bdbc2
connoromalley <william.omalley@umconnect.umt.edu>
2023-10-14 23:41:43 -0600

    works now

--- remove_element.py
+++ remove_element.py
@@ -1,9 +1,16 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        index = 0
+from typing import List 
 
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[index] = nums[i]
-                index += 1
-        return index 
+def removeElement(nums: List[int], val: int) -> int:
+    index = 0
+
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[index] = nums[i]
+            index += 1
+    return index 
+
+
+nums = [4, 3, 5, 6, 9]
+val = 9 
+length = removeElement(nums, val)
+print(nums[:length])
