d46d28e778e6be4f427f2a2253505ed0d2cfc5ed
connoromalley <william.omalley@umconnect.umt.edu>
2023-11-04 22:52:38 -0600

    added some examples

--- scrap.py
+++ scrap.py
@@ -1,7 +1,22 @@
-dummy = listNode() 
+@dataclass
+class ListNode:
+    val: int
+    next: Optional['ListNode'] = None
 
-cur = dummy
+# Create individual nodes
+node1 = ListNode(2)
+node2 = ListNode(4)
+node3 = ListNode(3)
 
-cur = 5
+# Link the nodes together to form a linked list
+node1.next = node2
+node2.next = node3
 
-print(dummy)
+# Now, node1 represents the head of the linked list
+head = node1
+
+# To access the elements of the linked list, you can iterate through it
+current = head
+while current is not None:
+    print(current.val)
+    current = current.next
