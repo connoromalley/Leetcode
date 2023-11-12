e5212f1ed8daf0a4ac447e6faa6ae10b1b28f8a7
connoromalley <william.omalley@umconnect.umt.edu>
2023-11-12 10:01:02 -0700

    added length of longest substring file

---
+++ lenght_of_longest_substring.py
@@ -0,0 +1,18 @@
+def length_sub(s:str) -> int:
+    seen = {}
+    left = 0
+    output = 0
+    for r in range(len(s)):
+        if s[r] not in seen:
+            output = max(output, r - left + 1)
+        else:
+            if seen[s[r]] < left:
+                output = max(output, r - left + 1)
+            else:
+                left = seen[s[r]] + 1
+        seen[s[r]] = r
+    return output
+
+s = 'abcebdabdcabcabc'
+result = length_sub(s)
+print(result) 
