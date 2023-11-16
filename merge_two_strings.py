f907cced60072c56d0f457f9f6ceec49a2a9fa3e
connoromalley <william.omalley@umconnect.umt.edu>
2023-11-15 22:38:42 -0700

    added comments

--- merge_two_strings.py
+++ merge_two_strings.py
@@ -2,10 +2,10 @@ class Solution:
     def merge(self, word1, word2):
         res=''
         for i in range(min(len(word1), len(word2))): # min(...) ensures we only interate up to the length of the shortest word
-            res += word1[i] + word2[i]
+            res += word1[i] + word2[i] # this concatenates each ith letter into res
 
-        return res + word1[i+1:] + word2[i+1:]
-    
+        return res + word1[i+1:] + word2[i+1:] # this appends the rest of whichever word was longer
+                                               # : goes to the end of the string 
 solution_instance = Solution()
 wordc = 'sadflkajslkdj'
 wordi = 'wewereiiieeeii'
