9225ab5b1bfde89a3682ea4cd60e57b46635fd32
connoromalley <william.omalley@umconnect.umt.edu>
2023-11-15 22:31:46 -0700

    comments added

--- merge_two_strings.py
+++ merge_two_strings.py
@@ -1,15 +1,15 @@
 class Solution:
     def merge(self, word1, word2):
         res=''
-        for i in range(min(len(word1), len(word2))):
+        for i in range(min(len(word1), len(word2))): # min(...) ensures we only interate up to the length of the shortest word
             res += word1[i] + word2[i]
 
         return res + word1[i+1:] + word2[i+1:]
     
 solution_instance = Solution()
-word1 = 'sadflkajslkdj'
-word2 = 'wewereiiieeeii'
+wordc = 'sadflkajslkdj'
+wordi = 'wewereiiieeeii'
 
 # call the merge method on the instance
-result = solution_instance.merge(word1, word2)
+result = solution_instance.merge(wordc, wordi)
 print(result)
