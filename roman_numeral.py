79b8e6f3cc1fba960b1d5ddfe5c8d558dc671abb
Connor O'Malley <89616719+connoromalley@users.noreply.github.com>
2024-04-07 08:43:28 -0600

    Update roman_numeral.py

--- roman_numeral.py
+++ roman_numeral.py
@@ -11,7 +11,7 @@ def romanToInt(s: str) -> int:
 
     ans = 0
     for i in range(len(s)):
-        if i < len(s) -1 and m[s[i]] < m[s[i+1]]:
+        if i < len(s) -1 and m[s[i]] < m[s[i+1]]: # we have to do lens(s) - 1 because we do s + 1 later and this keeps us in range
             ans -= m[s[i]]
         else:
             ans += m[s[i]]
